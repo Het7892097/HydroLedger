4d7947767859e95dda24947e533e78ffd7726636
Het <het41664@gmail.com>
2025-08-30 05:07:35 +0530

    Initialized supabase and google-login support

--- frontEnd/src/pages/Tab1.tsx
+++ frontEnd/src/pages/Tab1.tsx
@@ -1,6 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { Auth, SignIn,SocialAuth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
 import './Tab1.css';
+import { supabaseClient } from '../services/db/supabaseClient';
 
 const Tab1: React.FC = () => {
   return (
@@ -17,6 +21,14 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        {/* <IonButton onClick={SignIn}/> */}
+        {/* <SignIn children={<IonButton>Sign in with google</IonButton>} supabaseClient={supabaseClient} dark={false} /> */}
+         <Auth
+      supabaseClient={supabaseClient}
+      providers={['google']} // add other providers as needed
+      appearance={{ theme: ThemeSupa }}
+      // You can customize available social/email sign-in methods
+    />
       </IonContent>
     </IonPage>
   );
